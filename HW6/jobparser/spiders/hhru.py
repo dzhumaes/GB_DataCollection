ecef9b48edfcc1274c43bdae6507a1b87b1bc1ba
dzhumaes <dzhumaes@mail.ru>
2022-07-26 16:50:27 +0300

    Домашнее задание №6. Поправил путь по сборке городов с вакансий

--- HW6/jobparser/spiders/hhru.py
+++ HW6/jobparser/spiders/hhru.py
@@ -10,9 +10,9 @@ class HhruSpider(scrapy.Spider):
                   f'https://hh.ru/search/vacancy?area=2&search_field=name&search_field=company_name&search_field=description&text={search}&clusters=true&no_magic=true&ored_clusters=true&items_on_page=20&enable_snippets=true']
 
     def parse(self, response: HtmlResponse):
-        next_page = response.xpath("//a[@data-qa='pager-next']/@href").get()
-        if next_page:
-            yield response.follow(next_page, callback=self.parse)
+        # next_page = response.xpath("//a[@data-qa='pager-next']/@href").get()
+        # if next_page:
+        #     yield response.follow(next_page, callback=self.parse)
         links = response.xpath("//a[@data-qa='vacancy-serp__vacancy-title']/@href").getall()
         for link in links:
             yield response.follow(link, callback=self.vacancy_parse)
@@ -23,6 +23,6 @@ class HhruSpider(scrapy.Spider):
         name = response.xpath("//h1/text()").get()
         salary = response.xpath("//div[@data-qa='vacancy-salary']//text()").getall()
         url = response.url
-        city = response.xpath("//span[@data-qa='vacancy-view-raw-address']//text()").get()
+        city = response.xpath("//span[@data-qa='vacancy-view-raw-address']//text()|//p[@data-qa='vacancy-view-location']//text()").get()
         metro_name = response.xpath("//span[@class='metro-station']//text()").get()
         yield JobparserItem(name=name, salary=salary, url=url, city=city, metro_name=metro_name)
